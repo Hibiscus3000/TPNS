afce7757d805fb13dcdb69f2814c4be1e3ae843c
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2023-04-15 10:31:59 +0700

    lab2: made component cost function counting and perceptron logging

--- lab2/perceptron.py
+++ lab2/perceptron.py
@@ -1,4 +1,6 @@
 import numpy as np
+from logging import *
+
 
 class Perceptron:
     def __init__(self, layers, neurons, learning_rate):
@@ -28,12 +30,16 @@ class Perceptron:
             a.append(self.sigmoid(z[l - 1]))
         return z, a
 
-    # putput - output layer results
+    # output - output layer results
     def cost(self, output, y):
         C = 0  # C - cost function
         for i in range(0, len(y)):
             if (y[i] is not None):
                 C += (output[i] - y[i]) ** 2
+
+    def cost_component(self, output, y):
+        return [output[i] - y[i] if y[i] is not None else None for i in
+                range(0, len(y))]
 
     # z = Wa(L - 1) + b,a(L) = f(z), f - activation function, y - targets
     def back_prop(self, z, a, y):
@@ -57,21 +63,36 @@ class Perceptron:
 
         return dW, db
 
-    def calc_it(self, x, y):
+    def learn_it(self, x, y):
         z, a = self.forward_prop(x)
 
         dW, db = self.back_prop(z, a, y)
         for i in range(1, self.layers):
             self.W[i] -= self.learning_rate * dW[i]
             self.b[i] -= self.learning_rate * db[i]
-        print(self.cost(a[self.layers - 1],y))
+        return a[self.layers - 1]
 
     def learn(self, X, Y):
-        for i in range(0, len(X)):
-            self.calc_it(X[i], Y[i])
+        i = 0
+        for sample_id, x in X.items():
+            output = self.learn_it(X, Y[sample_id])
+            self.log_it_results(DEBUG if i % 5 else INFO, "learning", sample_id,
+                                output, Y[sample_id])
+            i += 1
 
     def predict(self, X, Y):
-        for i in range(0, len(X)):
-            _, a = self.forward_prop(X[i])
-            print(self.cost(a[self.layers - 1],Y[i]))
+        i = 0
+        for sample_id, x in X.items():
+            _, a = self.forward_prop(x)
+            self.log_it_results(DEBUG if i % 5 else INFO, "predicting", sample_id,
+                                a[self.layers - 1], Y[sample_id])
+            i += 1
 
+    def log_it_results(self, level, action_name, sample_id, output, y):
+        cost = self.cost(output, y)
+        cost_component = self.cost_component(output, y)
+        getLogger(__name__).log(level=level, msg=("%s %f...", action_name, sample_id))
+        getLogger(__name__).log(level=level, msg="y = [{}]".format(' '.join(map(str, y))))
+        getLogger(__name__).log(level=level, msg="output = [{}]".format(' '.join(map(str, y))))
+        getLogger(__name__).log(level=level, msg="cost: %f [{}]"
+                                .format(cost, ' '.join(map(str, cost_component))))
